822a506341fbf42ff89eeab8cfd75e29873218a0
Andrew W Bender <igothelp@gmail.com>
2011-08-30 01:55:08 -0500

    basic media page template

--- peace/media.php
+++ peace/media.php
@@ -5,45 +5,66 @@
 ?>
 
 <?php get_header(); ?>
-	<div id="content">
+	<div id='content'>
 		<?php if ( ! dynamic_sidebar( 'Alert' ) ) : ?>
 			<!--Wigitized 'Alert' for the home page -->
 		<?php endif ?>
-		<?php query_posts( 'post_type=peace_sermon'); ?>
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<div class="post-single">
-				<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-				<?php if ( has_post_thumbnail() ) { /* loades the post's featured thumbnail, requires Wordpress 3.0+ */ echo '<div class="featured-thumbnail">'; the_post_thumbnail(); echo '</div>'; } ?>
-				<div class="post-content">
-					<?php the_content(__('Read more'));?>
-				</div>
-				<div class="post-meta">
-					<p>
-						<?php the_time('F j, Y'); ?> at <?php the_time() ?>
-					</p>
-				</div><!--.postMeta-->
-			</div><!--.post-single-->
-		<?php endwhile; else: ?>
-			<div class="no-results">
-				<p><strong>There has been an error.</strong></p>
-				<p>We apologize for any inconvenience, please <a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('description'); ?>">return to the home page</a> or use the search form below.</p>
-				<?php get_search_form(); /* outputs the default Wordpress search form */ ?>
-			</div><!--noResults-->
-		<?php endif; ?>
-
-		<nav class="oldernewer">
-			<div class="older">
-				<p>
-					<?php next_posts_link('&laquo; Older Entries') ?>
-				</p>
-			</div><!--.older-->
-			<div class="newer">
-				<p>
-					<?php previous_posts_link('Newer Entries &raquo;') ?>
-				</p>
-			</div><!--.older-->
-		</nav><!--.oldernewer-->
-
+		<div>
+			<h2>Sermons</h2>
+			<?php query_posts( 'post_type=peace_sermon&post_status=publish&posts_per_page=5'); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<div class="post-single">
+					<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					<?php if ( has_post_thumbnail() ) { /* loades the post's featured thumbnail, requires Wordpress 3.0+ */ echo '<div class="featured-thumbnail">'; the_post_thumbnail(); echo '</div>'; } ?>
+					<div class="post-content">
+						<?php the_content(__('Read more'));?>
+					</div>
+					<div class="post-meta">
+						<p>
+							<?php the_time('F j, Y'); ?> at <?php the_time() ?>
+						</p>
+					</div><!--.postMeta-->
+				</div><!--.post-single-->
+			<a href='sermons'>View Archive</a>
+			<?php endwhile; endif; ?>
+		</div>
+		<div>
+			<h2>Congregation at Prayer</h2>
+			<?php query_posts( 'post_type=peace_congpray&post_status=publish&posts_per_page=5'); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<div class="post-single">
+					<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					<?php if ( has_post_thumbnail() ) { /* loades the post's featured thumbnail, requires Wordpress 3.0+ */ echo '<div class="featured-thumbnail">'; the_post_thumbnail(); echo '</div>'; } ?>
+					<div class="post-content">
+						<?php the_content(__('Read more'));?>
+					</div>
+					<div class="post-meta">
+						<p>
+							<?php the_time('F j, Y'); ?> at <?php the_time() ?>
+						</p>
+					</div><!--.postMeta-->
+				</div><!--.post-single-->
+			<a href='congregation-at-prayer'>View Archive</a>
+			<?php endwhile; endif; ?>
+		</div>
+		<div>
+			<h2>Bulletins</h2>
+			<?php query_posts( 'post_type=peace_bulletin&post_status=publish&posts_per_page=5'); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<div class="post-single">
+					<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					<?php if ( has_post_thumbnail() ) { /* loades the post's featured thumbnail, requires Wordpress 3.0+ */ echo '<div class="featured-thumbnail">'; the_post_thumbnail(); echo '</div>'; } ?>
+					<div class="post-content">
+						<?php the_content(__('Read more'));?>
+					</div>
+					<div class="post-meta">
+						<p>
+							<?php the_time('F j, Y'); ?> at <?php the_time() ?>
+						</p>
+					</div><!--.postMeta-->
+				</div><!--.post-single-->
+			<a href='bulletins'>View Archive</a>
+			<?php endwhile; endif; ?>
+		</div>
 	</div><!--#content-->
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
