2686cfcaea87b19595dddf2aba7c08d131fa75c2
blissdev <blissdev@gmail.com>
2011-08-31 19:56:28 -0500

    added header/footer to front-page template

--- peace/front-page.php
+++ peace/front-page.php
@@ -1,76 +1,77 @@
-<div id="top">
-  <div id="welcome">
-    <h3>Welcome</h3>
-    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin lobortis dictum tristique. Aliquam lacinia luctus interdum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin tempus dictum tincidunt. Morbi commodo ullamcorper nisl, sit amet lacinia elit pharetra.</p>
-  </div>
-</div> <!-- /top -->
+<?php get_header(); ?>
+  <div id="top">
+    <div id="welcome">
+      <h3>Welcome</h3>
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin lobortis dictum tristique. Aliquam lacinia luctus interdum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin tempus dictum tincidunt. Morbi commodo ullamcorper nisl, sit amet lacinia elit pharetra.</p>
+    </div>
+  </div> <!-- /top -->
 
-<div id="content">
-  <div id="mid" class="columns">
-    <div id="downloads" class="column">
-      <h3>Weekly Downloads</h3>
-      <h4>5th Sunday in Lent</h4>
-      <ul class="inside">
-        <li>Sermon <a href="#">Download</a></li>
-        <li>Bulletin <a href="#">Download</a></li>
-        <li>Congegration in Prayer <a href="#">Download</a></li>
-      </ul>
-    </div>
-    <div id="schedule" class="column">
-      <h3>Weekly Schedule</h3>
-      <h4>Divine Services &amp; Catechesis</h4>
-      <div class="inside columns">
-        <div class="column">
-          <h5>Divine Services</h5>
-          Sundays:  7:45am and 10:35am<br />
-          Wednesdays:  6:45pm<br />
-          Festivals, Feast Days, and Holidays
+  <div id="content">
+    <div id="mid" class="columns">
+      <div id="downloads" class="column">
+        <h3>Weekly Downloads</h3>
+        <h4>5th Sunday in Lent</h4>
+        <ul class="inside">
+          <li>Sermon <a href="#">Download</a></li>
+          <li>Bulletin <a href="#">Download</a></li>
+          <li>Congegration in Prayer <a href="#">Download</a></li>
+        </ul>
+      </div>
+      <div id="schedule" class="column">
+        <h3>Weekly Schedule</h3>
+        <h4>Divine Services &amp; Catechesis</h4>
+        <div class="inside columns">
+          <div class="column">
+            <h5>Divine Services</h5>
+            Sundays:  7:45am and 10:35am<br />
+            Wednesdays:  6:45pm<br />
+            Festivals, Feast Days, and Holidays
+          </div>
+          <div class="column">
+            <h5>Catechesis</h5>
+            Sundays:  9:15am Catechesis
+            Mondays:  7pm Didache
+            Thursdays:  9am Coffee Break Bible Study
+          </div>
         </div>
-        <div class="column">
-          <h5>Catechesis</h5>
-          Sundays:  9:15am Catechesis
-          Mondays:  7pm Didache
-          Thursdays:  9am Coffee Break Bible Study
+      </div>
+    </div> <!-- / mid -->
+    <div id="bottom" class="columns">
+      <div class="column">
+        <div class="header">
+          <h3>Announcements</h3>
+        </div>
+        <ul id="announcements">
+          <li>
+            <h5><a href="#">Plan for the Annual Flee Market</a></h5>
+            <p>Mark your calendar for our 7th annual Flea Market to benefit Peace Lutheran Church and Academy. Vendor information available now.</p>
+          </li>
+          <li>
+            <h5><a href="#">Joe Damoto released from the hospital!</a></h5>
+            <p>We give thanks to God for the Joe’s recovery and release from the hospital last week.</p>
+          </li>
+      </div>
+      <div class="column">
+        <div class="header">
+          <h3>Upcoming Events</h3>
+        </div>
+        <ul id="events">
+          <li>
+            <h5><a href="#">Fish Fry and Movie</a></h5>
+            <span class="date">Friday April 15th</span>
+            <p>Join us on Friday, April 15th for our monthly fish fry followed by viewing and theological discussion of The Passion of the Christ.</p>
+          </li>
+      </div>
+      <div class="column">
+        <div class="header">
+          <h3>Shepard of Peace</h3>
+          <h4>The Pastoral Blog of Peace Lutheran</h4>
+        </div>
+        <div class="lastest">
+          <h5><a href="#">Our Deliverance Is In the Cross</a></h5>
+          <p>When we think of the unique challenges we face in our lives as Christians (being faithful to our Lord, teaching our children the Word of God, bearing witness to our faith with family members, co-workers, and friends) it is easy to become discouraged.</p>
         </div>
       </div>
     </div>
-  </div> <!-- / mid -->
-  <div id="bottom" class="columns">
-    <div class="column">
-      <div class="header">
-        <h3>Announcements</h3>
-      </div>
-      <ul id="announcements">
-        <li>
-          <h5><a href="#">Plan for the Annual Flee Market</a></h5>
-          <p>Mark your calendar for our 7th annual Flea Market to benefit Peace Lutheran Church and Academy. Vendor information available now.</p>
-        </li>
-        <li>
-          <h5><a href="#">Joe Damoto released from the hospital!</a></h5>
-          <p>We give thanks to God for the Joe’s recovery and release from the hospital last week.</p>
-        </li>
-    </div>
-    <div class="column">
-      <div class="header">
-        <h3>Upcoming Events</h3>
-      </div>
-      <ul id="events">
-        <li>
-          <h5><a href="#">Fish Fry and Movie</a></h5>
-          <span class="date">Friday April 15th</span>
-          <p>Join us on Friday, April 15th for our monthly fish fry followed by viewing and theological discussion of The Passion of the Christ.</p>
-        </li>
-    </div>
-    <div class="column">
-      <div class="header">
-        <h3>Shepard of Peace</h3>
-        <h4>The Pastoral Blog of Peace Lutheran</h4>
-      </div>
-      <div class="lastest">
-        <h5><a href="#">Our Deliverance Is In the Cross</a></h5>
-        <p>When we think of the unique challenges we face in our lives as Christians (being faithful to our Lord, teaching our children the Word of God, bearing witness to our faith with family members, co-workers, and friends) it is easy to become discouraged.</p>
-      </div>
-    </div>
-  </div>
-</div> <!-- /content -->
-
+  </div> <!-- /content -->
+<?php get_footer(); ?>
