58fb3504f3d8bcb54c17a57e6f3147f27d97e466
blissdev <blissdev@gmail.com>
2011-08-31 19:24:14 -0500

    theming has begun

--- peace/header.php
+++ peace/header.php
@@ -1,71 +1,69 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<title><?php if ( is_category() ) {
-		echo 'Category Archive for &quot;'; single_cat_title(); echo '&quot; | '; bloginfo( 'name' );
-	} elseif ( is_tag() ) {
-		echo 'Tag Archive for &quot;'; single_tag_title(); echo '&quot; | '; bloginfo( 'name' );
-	} elseif ( is_archive() ) {
-		wp_title(''); echo ' Archive | '; bloginfo( 'name' );
-	} elseif ( is_search() ) {
-		echo 'Search for &quot;'.wp_specialchars($s).'&quot; | '; bloginfo( 'name' );
-	} elseif ( is_home() ) {
-		bloginfo( 'name' ); echo ' | '; bloginfo( 'description' );
-	}  elseif ( is_404() ) {
-		echo 'Error 404 Not Found | '; bloginfo( 'name' );
-	} elseif ( is_single() ) {
-		wp_title('');
-	} else {
-		echo wp_title(''); echo ' | '; bloginfo( 'name' );
-	} ?></title>
-	<meta name="description" content="<?php wp_title(''); echo ' | '; bloginfo( 'description' ); ?>" />
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<meta name="viewport" content="width=device-width; initial-scale=1"/><?php /* Add "maximum-scale=1" to fix the Mobile Safari auto-zoom bug on orientation changes, but keep in mind that it will disable user-zooming completely. Bad for accessibility. */ ?>
-	<link rel="icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" type="image/x-icon" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
-	<link rel="alternate" type="application/atom+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'atom_url' ); ?>" />
-	<?php wp_enqueue_script("jquery"); /* Loads jQuery if it hasn't been loaded already */ ?>
-	<?php /* The HTML5 Shim is required for older browsers, mainly older versions IE */ ?>
-	<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<?php wp_head(); ?> <?php /* this is used by many Wordpress features and for plugins to work proporly */ ?>
-	<?php /* Remove the Less Framework CSS line to not include the CSS Reset, basic styles/positioning, and Less Framework itself */?>
-	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/lessframework.css" />
-	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/theme.css" />
-	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-</head>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <meta name="viewport" content="width=device-width; initial-scale=1"/><?php /* Add "maximum-scale=1" to fix the Mobile Safari auto-zoom bug on orientation changes, but keep in mind that it will disable user-zooming completely. Bad for accessibility. */ ?>
+    <link rel="icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" type="image/x-icon" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
+    <link rel="alternate" type="application/atom+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'atom_url' ); ?>" />
+    <?php wp_enqueue_script("jquery"); /* Loads jQuery if it hasn't been loaded already */ ?>
 
-<body <?php body_class(); ?>>
-<div class="none">
-	<p><a href="#content">Skip to Content</a></p><?php /* used for accessibility, particularly for screen reader applications */ ?>
-</div><!--.none-->
-<div id="main"><!-- this encompasses the entire Web site -->
-	<div id="header"><header>
-		<div class="container">
-			<div id="title">
-				<?php if( is_front_page() || is_home() || is_404() ) { ?>
-					<h1 id="logo"><a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a></h1>
-					<h2 id="tagline"><?php bloginfo('description'); ?></h2>
-				<?php } else { ?>
-					<h2 id="logo"><a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a></h2>
-					<h3 id="tagline"><?php bloginfo('description'); ?></h3>
-				<?php } ?>
-			</div><!--#title-->
-			<div id="header-image" class="container">
-				<img src="<?php header_image(); ?>" width="<?php echo header_image_width; ?>" height="<?php echo header_image_height; ?>" alt="<?php bloginfo('name'); ?>" />
-			</div><!--#header-image-->
-			<div id="nav-primary" class="nav"><nav>
-				<?php if ( is_user_logged_in() ) {
-				     wp_nav_menu( array( 'theme_location' => 'logged-in-menu' ) ); /* if the visitor is logged in, this primary navigation will be displayed */
-				} else {
-				     wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); /* if the visitor is NOT logged in, this primary navigation will be displayed. if a single menu should be displayed for both conditions, set the same menues to be displayed under both conditions through the Wordpress backend */
-				} ?>
-			</nav></div><!--#nav-primary-->
-			<?php if ( ! dynamic_sidebar( 'Header' ) ) : ?><!-- Wigitized Header --><?php endif ?>
-			<div class="clear"></div>
-		</div><!--.container-->
-	</header></div><!--#header-->
-	<div class="container">
+    <link type="text/css" rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/stylesheets/normalize.css" media="screen" />
+    <link type="text/css" rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/stylesheets/site.css" media="screen" />
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <title><?php if ( is_category() ) {
+      echo 'Category Archive for &quot;'; single_cat_title(); echo '&quot; | '; bloginfo( 'name' );
+    } elseif ( is_tag() ) {
+      echo 'Tag Archive for &quot;'; single_tag_title(); echo '&quot; | '; bloginfo( 'name' );
+    } elseif ( is_archive() ) {
+      wp_title(''); echo ' Archive | '; bloginfo( 'name' );
+    } elseif ( is_search() ) {
+      echo 'Search for &quot;'.wp_specialchars($s).'&quot; | '; bloginfo( 'name' );
+    } elseif ( is_home() ) {
+      bloginfo( 'name' ); echo ' | '; bloginfo( 'description' );
+    }  elseif ( is_404() ) {
+      echo 'Error 404 Not Found | '; bloginfo( 'name' );
+    } elseif ( is_single() ) {
+      wp_title('');
+    } else {
+      echo wp_title(''); echo ' | '; bloginfo( 'name' );
+    } ?></title>
+
+    <script type="text/javascript" src="http://use.typekit.com/ltq7dhs.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    <?php wp_head(); ?>
+  </head>
+  
+  <body class="<?php body_class(); ?>">
+
+    <section id="main" role="main">
+      <header>
+        <div id="title">
+          <h1><a href="#">Peace Lutheran Church</a></h1>
+          <span class="loc">Sussex, Wisconsin</span>
+        </div>
+        <nav role="navigation">
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Catechesis</a></li>
+            <li><a href="#">Media</a></li>
+            <li><a href="#">Calendar</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </nav>
+        <ul id="site-selector">
+          <li><a href="#">Academy</a></li>
+          <li><a href="#">CCA</a></li>
+          <li class="at"><a href="#">Parish</a></li>
+        </ul>
+      </header>
+      <article id="content">
+
+
