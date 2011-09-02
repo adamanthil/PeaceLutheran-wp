c5554172a36d1a5bfea60f7d65b4207fa4de5bf0
Andrew W Bender <igothelp@gmail.com>
2011-09-02 00:40:01 -0500

    simplifying sidebar

--- peace/sidebar.php
+++ peace/sidebar.php
@@ -6,26 +6,10 @@
 			<?php get_search_form(); /* outputs the default Wordpress search form */ ?>
 		</li>
 		
-		<li id="sidebar-nav" class="widget menu">
-			<h3>Navigation</h3>
-			<ul>
-				<?php wp_nav_menu( array( 'theme_location' => 'sidebar-menu' ) ); /* editable within the Wordpress backend */ ?>
-			</ul>
-		</li>
-		
 		<li id="sidebar-archives" class="widget">
 			<h3>Archives</h3>
 			<ul>
 				<?php wp_get_archives( 'type=monthly' ); ?>
-			</ul>
-		</li>
-
-		<li id="sidebar-meta" class="widget">
-			<h3>Meta</h3>
-			<ul>
-				<?php wp_register(); ?>
-				<li><?php wp_loginout(); ?></li>
-				<?php wp_meta(); ?>
 			</ul>
 		</li>
 
