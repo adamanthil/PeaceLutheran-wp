410481398c39a951b41b63153a415c9ac8522f4e
blissdev <blissdev@gmail.com>
2011-09-01 19:11:23 -0500

    more updates

--- peace/index.php
+++ peace/index.php
@@ -14,7 +14,7 @@
 					<p>
 						Written on <?php the_time('F j, Y'); ?> at <?php the_time() ?>, by <?php the_author_posts_link() ?>
 					</p>
-					<p>
+					<p class="meta">
 						<?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?>
 						<br />
 						Categories: <?php the_category(', ') ?>
